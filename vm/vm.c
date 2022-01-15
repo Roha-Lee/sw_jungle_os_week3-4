3e51b67f090b48e7fd8f462995a10cda69efa446
Roha Lee <rohagru@gmail.com>
2022-01-15 01:53:52 +0000

    1. header 선언 2. spt 구조체 내부에 hash생성 3. page구조체 내부에 hash_elem멤버 생성 4. page_hash, pass_less구현 5. supplemental_page_table_init구현

--- vm/vm.c
+++ vm/vm.c
@@ -4,6 +4,23 @@
 #include "vm/vm.h"
 #include "vm/inspect.h"
 
+/* Returns a hash value for page p. */
+unsigned
+page_hash (const struct hash_elem *p_, void *aux UNUSED) {
+  const struct page *p = hash_entry (p_, struct page, hash_elem);
+  return hash_bytes (&p->addr, sizeof p->addr);
+}
+
+/* Returns true if page a precedes page b. */
+bool
+page_less (const struct hash_elem *a_,
+           const struct hash_elem *b_, void *aux UNUSED) {
+  const struct page *a = hash_entry (a_, struct page, hash_elem);
+  const struct page *b = hash_entry (b_, struct page, hash_elem);
+
+  return a->addr < b->addr;
+}
+
 /* Initializes the virtual memory subsystem by invoking each subsystem's
  * intialize codes. */
 void
@@ -174,6 +191,7 @@ vm_do_claim_page (struct page *page) {
 /* Initialize new supplemental page table */
 void
 supplemental_page_table_init (struct supplemental_page_table *spt UNUSED) {
+	
 }
 
 /* Copy supplemental page table from src to dst */
